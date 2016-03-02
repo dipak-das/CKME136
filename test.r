2936a7c9b37ef4d62a6687ba413f7df592869859
dipak-das <dipak_das_1999@yahoo.com>
2016-03-01 22:51:04 -0500

    Added files via upload

--- test.r
+++ test.r
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library("RSQLite") 
 library("tm")
 library("RCurl")
@@ -7,8 +6,3 @@ library("klaR")
 
 library(plyr)
 library(stringr)
-=======
-library("RSQLite") 
-library("tm")
-library("RCurl")
->>>>>>> 373eb664ddaabbb73328d26ee6db043d0ac68eaa
